352df35f28db26af1c522f0f8f029c5198f8b130
Aryan1 Kumar <aryan11061@gmail.com>
2024-12-28 00:22:35 +0530

    screenshot utility

--- src/test/java/testScriptt/Basictest.java
+++ src/test/java/testScriptt/Basictest.java
@@ -5,17 +5,41 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import fbPageObject.LoginPageObjectclss;
+import utilitypkg.Screenshotutility;
 public class Basictest extends GenericTestforall{
 
-	 @Test
+	
+	@Test
 	public void m1() throws IOException {
 		
 		// launchandLogin();
 		 tryloginandgotoreg(); 
 		 doRegistarion(); 
+		 
+		 //Assert.assertTrue(false);
 		 }
 
+	 
+	 
+	 @AfterMethod
+	 
+	 
+	 public void breakDown(ITestResult result) throws IOException {
+		 
+		 
+		 if(ITestResult.FAILURE==result.getStatus()) {
+			 
+			 Screenshotutility su=new Screenshotutility(driver);
+			 su.techScreenshot(driver); 
+		 }
+		 
+		 
+	 }
+	 
 }
